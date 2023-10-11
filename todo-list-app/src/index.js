356e28954bb5b9162bc840b808ec27504805b572
JoyEmma12 <ejoy0600@gmail.com>
2023-10-11 02:21:27 +0100

    onboarding screen completed

--- todo-list-app/src/index.js
+++ todo-list-app/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import MainScreen from './mainScreen';
+
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/*const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MainScreen />
   </React.StrictMode>
-);
+);*/
+const root1 = ReactDOM.createRoot(document.getElementById('root1'));
+root1.render(
+  <MainScreen />
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ todo-list-app/src/mainScreen.js
@@ -0,0 +1,17 @@
+import './todoapp.css';
+function MainScreen() {
+    return (
+        <>
+            <div className='mainTitle'>
+                <h1 className="mainScreenHeader">
+                    Turn Chaos into Clarity: Streamline Your Tasks with Us!
+                </h1>
+
+                <div>
+                    <button type="submit" className="mainBtn">Get Started</button>
+                </div>
+            </div>
+        </>
+    );
+}
+export default MainScreen;
